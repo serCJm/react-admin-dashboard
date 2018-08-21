2f8b7dd2ed8f2ab13a280865b73cdc0a24a3879e
CJ Moro <scjmoro@gmail.com>
2018-08-21 07:08:52 -0700

    Add components home menu section

--- src/components/SideMenuHomeComponents.jsx
+++ src/components/SideMenuHomeComponents.jsx
@@ -7,6 +7,32 @@ import { faChartLine } from "@fortawesome/free-solid-svg-icons";
 import { faChartBar } from "@fortawesome/free-solid-svg-icons";
 import { faChartArea } from "@fortawesome/free-solid-svg-icons";
 import { faSignal } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faStopwatch } from "@fortawesome/free-solid-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
+import { faCompass } from "@fortawesome/free-solid-svg-icons";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { faTable } from "@fortawesome/free-solid-svg-icons";
+import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
+import { faAdjust } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+import { faHandPointRight } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faMagic } from "@fortawesome/free-solid-svg-icons";
+import { faTv } from "@fortawesome/free-solid-svg-icons";
+import { faCompactDisc } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faThLarge } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faImages } from "@fortawesome/free-solid-svg-icons";
+import { faFont } from "@fortawesome/free-solid-svg-icons";
 
 class SideMenuHomeComponents extends Component {
   constructor(props) {
@@ -75,34 +101,159 @@ class SideMenuHomeComponents extends Component {
           ]
         },
         {
-          id: "gallery",
-          icon: <FontAwesomeIcon icon={null} />,
-          text: "Gallery",
+          id: "static-timeline",
+          icon: <FontAwesomeIcon icon={faClock} />,
+          text: "Static Timeline",
           submenu: null
         },
         {
-          id: "social",
-          icon: <FontAwesomeIcon icon={null} />,
-          text: "Social",
+          id: "interactive-timeline",
+          icon: <FontAwesomeIcon icon={faStopwatch} />,
+          text: "Interactive Timeline",
           submenu: null
         },
         {
-          id: "blog",
-          icon: <FontAwesomeIcon icon={null} />,
-          text: "Blog",
-          countstyle: { color: "#fff", background: "rgb(46, 179, 152)" },
+          id: "codemirror",
+          icon: <FontAwesomeIcon icon={faCode} />,
+          text: "Codemirror",
+          submenu: null
+        },
+        {
+          id: "maps",
+          icon: <FontAwesomeIcon icon={faMapMarkerAlt} />,
+          text: "Maps",
+          submenu: null
+        },
+        {
+          id: "editor",
+          icon: <FontAwesomeIcon icon={faPenSquare} />,
+          text: "Editor",
+          submenu: null
+        },
+        {
+          id: "ui-elements",
+          icon: <FontAwesomeIcon icon={faCompass} />,
+          text: "UI Elements",
+          countstyle: { color: "#fff", background: "rgb(215, 31, 75)" },
           submenu: [
             {
-              id: "posts",
-              icon: <FontAwesomeIcon icon={null} />,
-              text: "Posts"
+              id: "buttons",
+              icon: <FontAwesomeIcon icon={faPlayCircle} />,
+              text: "Buttons"
             },
             {
-              id: "singlePost",
-              icon: <FontAwesomeIcon icon={null} />,
-              text: "Single Post"
+              id: "dropdowns",
+              icon: <FontAwesomeIcon icon={faChevronCircleDown} />,
+              text: "Dropdowns"
+            },
+            {
+              id: "tabs-navs",
+              icon: <FontAwesomeIcon icon={faTable} />,
+              text: "Tabs & Navs"
+            },
+            {
+              id: "sliders",
+              icon: <FontAwesomeIcon icon={faSlidersH} />,
+              text: "Sliders"
+            },
+            {
+              id: "knobs",
+              icon: <FontAwesomeIcon icon={faAdjust} />,
+              text: "Knobs"
+            },
+            {
+              id: "modals",
+              icon: <FontAwesomeIcon icon={faBars} />,
+              text: "Modals"
+            },
+            {
+              id: "messanger",
+              icon: <FontAwesomeIcon icon={faComment} />,
+              text: "Messanger"
             }
           ]
+        },
+        {
+          id: "forms",
+          icon: <FontAwesomeIcon icon={faHandPointRight} />,
+          text: "Forms",
+          countstyle: { color: "#fff", background: "rgb(217, 83, 79)" },
+          submenu: [
+            {
+              id: "controls",
+              icon: <FontAwesomeIcon icon={faGamepad} />,
+              text: "Controls"
+            },
+            {
+              id: "x-editable",
+              icon: <FontAwesomeIcon icon={faEdit} />,
+              text: "X-Editable"
+            },
+            {
+              id: "wizard",
+              icon: <FontAwesomeIcon icon={faMagic} />,
+              text: "Wizard"
+            }
+          ]
+        },
+        {
+          id: "tables",
+          icon: <FontAwesomeIcon icon={faTable} />,
+          text: "Tables",
+          countstyle: { color: "#fff", background: "rgb(121, 176, 236)" },
+          submenu: [
+            {
+              id: "bootstrap",
+              icon: <FontAwesomeIcon icon={faTv} />,
+              text: "Bootstrap Tables"
+            },
+            {
+              id: "datatables",
+              icon: <FontAwesomeIcon icon={faCompactDisc} />,
+              text: "Datatables"
+            },
+            {
+              id: "tablesaw",
+              icon: <FontAwesomeIcon icon={faSun} />,
+              text: "Tablesaw"
+            }
+          ]
+        },
+        {
+          id: "grid",
+          icon: <FontAwesomeIcon icon={faThLarge} />,
+          text: "Grid",
+          submenu: null
+        },
+        {
+          id: "calendar",
+          icon: <FontAwesomeIcon icon={faCalendarAlt} />,
+          text: "Calendar",
+          submenu: null
+        },
+        {
+          id: "file-utilities",
+          icon: <FontAwesomeIcon icon={faFile} />,
+          text: "File Utilities",
+          countstyle: { color: "#fff", background: "rgb(255, 196, 151)" },
+          submenu: [
+            {
+              id: "dropzone",
+              icon: <FontAwesomeIcon icon={faDownload} />,
+              text: "Dropzone"
+            },
+            {
+              id: "image-cropping",
+              icon: <FontAwesomeIcon icon={faImages} />,
+              text: "Image Cropping"
+            }
+          ]
+        },
+        {
+          id: "fonts",
+          icon: <FontAwesomeIcon icon={faFont} />,
+          text: "Fonts",
+          submenu: null
         }
       ],
       classNames: {

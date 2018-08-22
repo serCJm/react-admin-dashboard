accf1c177dbc609b9d8ef60ee82660ec33681002
CJ Moro <scjmoro@gmail.com>
2018-08-21 18:22:43 -0700

    Add home menu extras content

--- src/components/SideMenuHome.jsx
+++ src/components/SideMenuHome.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SideMenuHomePages from "./SideMenuHomePages";
 import SideMenuHomeComponents from "./SideMenuHomeComponents";
+import SideMenuHomeExtras from "./SideMenuHomeExtras";
 
 class SideMenuHome extends Component {
   constructor(props) {
@@ -24,6 +25,10 @@ class SideMenuHome extends Component {
           currentMenu={this.state.currentMenu}
         />
         <SideMenuHomeComponents
+          handleActivePage={this.handleActivePage}
+          currentMenu={this.state.currentMenu}
+        />
+        <SideMenuHomeExtras
           handleActivePage={this.handleActivePage}
           currentMenu={this.state.currentMenu}
         />
---
+++ src/components/SideMenuHomeExtras.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import SideMenuHomeList from "./SideMenuHomeList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faUserLock } from "@fortawesome/free-solid-svg-icons";
+import { faReceipt } from "@fortawesome/free-solid-svg-icons";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
+class SideMenuHomeExtras extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: [
+        {
+          id: "login",
+          icon: <FontAwesomeIcon icon={faSignInAlt} />,
+          text: "Login",
+          submenu: null
+        },
+        {
+          id: "signup",
+          icon: <FontAwesomeIcon icon={faUserPlus} />,
+          text: "Sign Up",
+          submenu: null
+        },
+        {
+          id: "lock-page",
+          icon: <FontAwesomeIcon icon={faUserLock} />,
+          text: "Lock Page",
+          submenu: null
+        },
+        {
+          id: "invoice",
+          icon: <FontAwesomeIcon icon={faReceipt} />,
+          text: "Invoice",
+          submenu: null
+        },
+        {
+          id: "pricing-tables",
+          icon: <FontAwesomeIcon icon={faDollarSign} />,
+          text: "Pricing Tables",
+          submenu: null
+        }
+      ],
+      classNames: {
+        title: "title",
+        menuItem: "menu-item",
+        text: "text",
+        subParent: "submenu-parent",
+        subMenu: "sub-menu",
+        subMenuCount: "submenu-count",
+        menuIcon: "menu-icon",
+        hover: "hover",
+        active: "active"
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className={this.state.classNames.title}>
+          {Object.keys(this.state)[0]}
+        </h2>
+        <SideMenuHomeList
+          listItems={this.state.pages}
+          classNames={this.state.classNames}
+          handleActivePage={this.props.handleActivePage}
+          currentMenu={this.props.currentMenu}
+        />
+      </div>
+    );
+  }
+}
+export default SideMenuHomeExtras;

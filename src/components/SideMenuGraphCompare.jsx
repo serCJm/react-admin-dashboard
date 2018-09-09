2138bf535c526a4e9f6ed5c2a643381df074e083
CJ Moro <scjmoro@gmail.com>
2018-09-09 07:24:38 -0700

    Remove import component

--- src/components/SideMenuGraphCompare.jsx
+++ src/components/SideMenuGraphCompare.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import StackedBarChart from "./graphs/StackedBarChart";
 import SideMenuGraphCompareCount from "./SideMenuGraphCompareCount";
 

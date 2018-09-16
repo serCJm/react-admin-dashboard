8cf7c4861aa6c7060a35467bee700feb698a4372
CJ Moro <scjmoro@gmail.com>
2018-09-15 23:09:27 -0700

    Fix bay-theme spelling

---
+++ src/components/Header/theme-context.js
@@ -0,0 +1,13 @@
+import React from "react";
+export const themes = {
+  default: "theme-default",
+  green: "theme-green",
+  blue: "theme-blue",
+  purple: "theme-purple",
+  brown: "theme-brown",
+  bay: "theme-bay"
+};
+
+export const ThemeContext = React.createContext(
+  themes.default // default value
+);

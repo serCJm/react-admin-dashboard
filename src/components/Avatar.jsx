79df40e74b2d281f02c286318b2b27737a8cfe0e
CJ Moro <scjmoro@gmail.com>
2018-09-17 22:44:41 -0700

    Add theme context

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,26 +2,31 @@ import React from "react";
 import imageAvatar from "../images/avatar.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { ThemeContext } from "./Header/theme-context";
 
-function Avatar(props) {
+function Avatar() {
   return (
-    <div className="avatar">
-      <img
-        className="avatar-img"
-        src={imageAvatar}
-        style={{ width: 50, height: 50 }}
-        alt="admin"
-      />
-      <div className="avatar-info">
-        <h2 className="avatar-name">CJ Moro</h2>
-        <div className="avatar-progress-bar field">
-          <div className="avatar-progress-bar fill" />
+    <ThemeContext.Consumer>
+      {({ theme }) => (
+        <div className={`avatar ${theme}`}>
+          <img
+            className="avatar-img"
+            src={imageAvatar}
+            style={{ width: 50, height: 50 }}
+            alt="admin"
+          />
+          <div className="avatar-info">
+            <h2 className="avatar-name">CJ Moro</h2>
+            <div className={`avatar-progress-bar field ${theme}`}>
+              <div className="avatar-progress-bar fill" />
+            </div>
+          </div>
+          <span className={`avatar-lock ${theme}`}>
+            <FontAwesomeIcon icon={faCoffee} />
+          </span>
         </div>
-      </div>
-      <span className="avatar-lock">
-        <FontAwesomeIcon icon={faCoffee} />
-      </span>
-    </div>
+      )}
+    </ThemeContext.Consumer>
   );
 }
 

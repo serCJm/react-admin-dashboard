23f23071a52a1fb60df0f1b2de4d178c1b4c7f94
CJ Moro <scjmoro@gmail.com>
2018-09-17 20:23:25 -0700

    Add context provider

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import HeaderTopMenu from "../components/Header/HeaderTopMenu";
 import SideMenu from "../components/SideMenu/SideMenu";
 import HeaderMenuBtn from "../components/Header/HeaderMenuBtn";
+import { ThemeContext, themes } from "../components/Header/theme-context";
 
 class Header extends Component {
   state = {
-    mobile: window.matchMedia("(max-width: 800px)").matches
+    mobile: window.matchMedia("(max-width: 800px)").matches,
+    theme: themes.default
   };
 
   hangleScreenResize = () => {
@@ -22,20 +24,21 @@ class Header extends Component {
   }
 
   render() {
-    console.log(window.matchMedia("(max-width: 800px)"));
     let headerMenuBtn = null;
     if (this.state.mobile) {
       headerMenuBtn = <HeaderMenuBtn onClick={this.props.toggleSideMenu} />;
     }
     return (
       <header>
-        <HeaderTopMenu
-          logo="Logo"
-          headerMenuBtn={headerMenuBtn}
-          sideMenu={this.props.sideMenu}
-          mobile={this.state.mobile}
-        />
-        <SideMenu sideMenu={this.props.sideMenu} mobile={this.state.mobile} />
+        <ThemeContext.Provider value={this.state.theme}>
+          <HeaderTopMenu
+            logo="Logo"
+            headerMenuBtn={headerMenuBtn}
+            sideMenu={this.props.sideMenu}
+            mobile={this.state.mobile}
+          />
+          <SideMenu sideMenu={this.props.sideMenu} mobile={this.state.mobile} />
+        </ThemeContext.Provider>
       </header>
     );
   }

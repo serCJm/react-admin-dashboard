c49422cdb96e2b8997952fa9488ba06c24a040a7
CJ Moro <scjmoro@gmail.com>
2018-09-17 20:24:23 -0700

    Add context consume and change to sfc

--- src/components/Header/HeaderLogoutBtn.jsx
+++ src/components/Header/HeaderLogoutBtn.jsx
@@ -1,36 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { ThemeContext } from "./theme-context";
 
-class HeaderLogoutBtn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isHoverOn: false };
-
-    this.handleHover = this.handleHover.bind(this);
-  }
-
-  handleHover() {
-    this.setState(prevState => ({ isHoverOn: !prevState.isHoverOn }));
-  }
-  render() {
-    return (
-      <button
-        className="menu-btn"
-        onMouseEnter={this.handleHover}
-        onMouseLeave={this.handleHover}
-        style={
-          this.state.isHoverOn
-            ? { background: "rgb(234, 118, 97)" }
-            : { background: "rgba(231, 96, 73, 1)" }
-        }
-      >
-        <span className="menu-btn-icon">
-          <FontAwesomeIcon icon={faSignOutAlt} />
-        </span>
-      </button>
-    );
-  }
-}
+const HeaderLogoutBtn = () => {
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <button className={`menu-btn ${theme}`}>
+          <span className="menu-btn-icon">
+            <FontAwesomeIcon icon={faSignOutAlt} />
+          </span>
+        </button>
+      )}
+    </ThemeContext.Consumer>
+  );
+};
 
 export default HeaderLogoutBtn;

5f2c287740fe30a711517e1958531b0a00dca8c1
CJ Moro <scjmoro@gmail.com>
2018-09-24 22:42:06 -0700

    Add position class prop

--- src/components/Header/HeaderHoverMenu.jsx
+++ src/components/Header/HeaderHoverMenu.jsx
@@ -5,7 +5,7 @@ const HeaderHoverMenu = props => {
   return (
     <ThemeContext>
       {({ theme }) => (
-        <div className={`hover-menu ${theme}`}>
+        <div className={`hover-menu ${theme} ${props.positionClass}`}>
           <h1 className="hover-menu-title">{props.heading}</h1>
           {props.children}
         </div>

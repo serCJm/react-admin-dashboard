6f27a4c674d2915f37f49fbb49fc274b42d6a1bf
CJ Moro <scjmoro@gmail.com>
2018-09-30 23:41:48 -0700

    Add option for a custom class

--- src/components/graphs/tooltips/TooltipBox.jsx
+++ src/components/graphs/tooltips/TooltipBox.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const TooltipBox = props => {
   return (
-    <div style={props.position} className="tooltip-box">
+    <div
+      style={props.position}
+      className={props.tooltipClass ? props.tooltipClass : "tooltip-box"}
+    >
       <p className="text">{props.text1}</p>
       {props.text2 ? <p className="text">{props.text2}</p> : null}
     </div>

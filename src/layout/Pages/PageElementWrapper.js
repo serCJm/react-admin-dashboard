7b8414d57d71b74683a7584b10c70b7a034d6d4a
CJ Moro <scjmoro@gmail.com>
2018-10-06 22:40:47 -0700

    Complete PageElementWrapper component

---
+++ src/layout/Pages/PageElementWrapper.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+
+class PageElementWrapper extends Component {
+  state = {
+    controlsMenu: false,
+    minimize: false,
+    showElement: true
+  };
+
+  handleHover = () => {
+    this.setState(prevState => ({ controlsMenu: !prevState.controlsMenu }));
+  };
+
+  handleMinimize = () => {
+    this.setState(prevState => ({ minimize: !prevState.minimize }));
+  };
+
+  handleClose = () => {
+    this.setState(prevState => ({ showElement: !prevState.showElement }));
+  };
+
+  render() {
+    let controlsMenuClass = "page-element-controls";
+    if (this.state.controlsMenu) {
+      controlsMenuClass += " show";
+    }
+    let pageElementClass = "page-element";
+    if (this.state.minimize) {
+      pageElementClass += " minimize";
+    }
+    return (
+      <div
+        className="page-element-container"
+        onMouseOver={this.handleHover}
+        onMouseOut={this.handleHover}
+      >
+        {this.state.showElement ? (
+          <div className={pageElementClass}>
+            <div className={controlsMenuClass}>
+              <span className="page-element-controls-item">
+                <FontAwesomeIcon icon={faSyncAlt} />
+              </span>
+              <span
+                className="page-element-controls-item"
+                onClick={this.handleMinimize}
+              >
+                -
+              </span>
+              <span
+                className="page-element-controls-item"
+                onClick={this.handleClose}
+              >
+                &times;
+              </span>
+            </div>
+            {this.props.children}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default PageElementWrapper;

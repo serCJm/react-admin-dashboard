ce433440eb3de2a11f160030f4cde7c12e6053ce
CJ Moro <scjmoro@gmail.com>
2018-10-11 22:10:22 -0700

    Add responsive layout logic

--- src/layout/Pages/Pages.jsx
+++ src/layout/Pages/Pages.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Dashboard from "./Dashboard/Dashboard";
 import { Route } from "react-router-dom";
 
-const Pages = ({ sideMenu }) => {
-  let mainClass = "pages sidemenu-open";
-  if (!sideMenu && window.matchMedia("(max-width: 800px)").matches) {
-    mainClass = "pages";
+class Pages extends Component {
+  state = {
+    mobile: window.matchMedia("(max-width: 800px)").matches
+  };
+
+  hangleScreenResize = () => {
+    const mq = window.matchMedia("(max-width: 800px)").matches;
+    this.setState({ mobile: mq });
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.hangleScreenResize);
   }
-  return (
-    <main className={mainClass}>
-      <Route exact path="/" component={Dashboard} />
-    </main>
-  );
-};
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.hangleScreenResize);
+  }
+
+  render() {
+    const { sideMenu } = this.props;
+    let mainClass = "pages sidemenu-open";
+    if (!sideMenu && window.matchMedia("(max-width: 800px)").matches) {
+      mainClass = "pages";
+    }
+    return (
+      <main className={mainClass}>
+        <div className="dashboard">
+          <Route exact path="/" component={Dashboard} />
+        </div>
+      </main>
+    );
+  }
+}
 
 export default Pages;

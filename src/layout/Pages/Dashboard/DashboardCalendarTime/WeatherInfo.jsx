9e81e28fad63939b0db6441661d1eb5d877327fd
CJ Moro <scjmoro@gmail.com>
2018-11-07 21:43:54 -0800

    Add weather component

--- src/layout/Pages/Dashboard/DashboardCalendarTime/WeatherInfo.jsx
+++ src/layout/Pages/Dashboard/DashboardCalendarTime/WeatherInfo.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 
 const WeatherInfo = () => {
-  return <React.Fragment />;
+  return (
+    <div className="weather">
+      <p className="weather-icon">
+        <FontAwesomeIcon icon={faCloudRain} />
+      </p>
+      <div className="weather-temp">
+        <p>Max: 25&#xb0;</p>
+        <p>Min: 21&#xb0;</p>
+      </div>
+      <p className="weather-status">Showers</p>
+      <p className="weather-stats">Wind: 19 km/h | Humidity: 87%</p>
+    </div>
+  );
 };
 
 export default WeatherInfo;

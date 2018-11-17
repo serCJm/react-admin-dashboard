432ed501387002b202307be87b395a3185557f05
CJ Moro <scjmoro@gmail.com>
2018-11-17 13:51:15 -0800

    Add a link to mail component

--- src/layout/Pages/Mailbox/Inbox/InboxMail/InboxMailItem.jsx
+++ src/layout/Pages/Mailbox/Inbox/InboxMail/InboxMailItem.jsx
@@ -1,37 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const InboxMailItem = ({ contents }) => {
   return (
     <ul className="inbox-mail">
       {contents.map((item, i) => (
-        <li key={item.name + 1} className="inbox-mail-item">
-          <div className="inbox-mail-main">
-            <img
-              className={`inbox-mail-avatar ${item.labelStyle}`}
-              src={item.avatar}
-              width={50}
-              height={50}
-              alt={item.name}
-            />
-            <div>
-              <h3>
-                {item.name}
-                {item.count ? <span> ({item.count})</span> : null}
-              </h3>
+        <Link to="/mail" key={item.name + 1}>
+          <li className="inbox-mail-item">
+            <div className="inbox-mail-main">
+              <img
+                className={`inbox-mail-avatar ${item.labelStyle}`}
+                src={item.avatar}
+                width={50}
+                height={50}
+                alt={item.name}
+              />
+              <div>
+                <h3>
+                  {item.name}
+                  {item.count ? <span> ({item.count})</span> : null}
+                </h3>
 
-              <p className="inbox-mail-text">
-                <span className={`inbox-mail-label ${item.labelStyle}`}>
-                  {item.labelText}
-                </span>
-                {item.mailContent}
-              </p>
+                <p className="inbox-mail-text">
+                  <span className={`inbox-mail-label ${item.labelStyle}`}>
+                    {item.labelText}
+                  </span>
+                  {item.mailContent}
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="inbox-mail-date">
-            <p>{item.date}</p>
-            <p>#{i + 1}</p>
-          </div>
-        </li>
+            <div className="inbox-mail-date">
+              <p>{item.date}</p>
+              <p>#{i + 1}</p>
+            </div>
+          </li>
+        </Link>
       ))}
     </ul>
   );

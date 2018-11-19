59bc2eca78e8791e207ea95047a1ef700a05103d
CJ Moro <scjmoro@gmail.com>
2018-11-19 06:21:50 -0800

    Add Link to Mail component

--- src/components/Header/HeaderControlsNotifications.jsx
+++ src/components/Header/HeaderControlsNotifications.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
@@ -72,9 +73,11 @@ class HeaderControlsNotifications extends Component {
     }
     return (
       <div className="header-controls-item">
-        <span className="header-controls-icon">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </span>
+        <Link className="header-controls-icon" to="/inbox">
+          <span>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+        </Link>
         <span
           className="header-controls-icon"
           ref={node => {

62bc1529307efede6544a4fafc4cda46809fec57
CJ Moro <scjmoro@gmail.com>
2018-11-24 06:51:27 -0800

    Add SocialDayPlan component

--- src/layout/Pages/Social/Social.jsx
+++ src/layout/Pages/Social/Social.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import SocialHeader from "./SocialHeader/SocialHeader";
 import SocialThoughts from "./SocialThoughts/SocialThoughts";
+import SocialDayPlan from "./SocialDayPlan/SocialDayPlan";
 
 const Social = () => {
   return (
     <div className="social">
       <SocialHeader />
       <SocialThoughts />
-      {/* <SocialDayPlan />
-      <SocialEvent />
+      <SocialDayPlan />
+      {/* <SocialEvent />
       <SocialMusicClip /> */}
     </div>
   );

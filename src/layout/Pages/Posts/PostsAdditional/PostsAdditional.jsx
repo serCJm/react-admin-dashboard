1d5e2f71a07e543fb35b7d3491504acb7ff87603
CJ Moro <scjmoro@gmail.com>
2018-12-01 16:23:15 -0800

    Create TradingPosts component

--- src/layout/Pages/Posts/PostsAdditional/PostsAdditional.jsx
+++ src/layout/Pages/Posts/PostsAdditional/PostsAdditional.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import AvatarIntro from "./AvatarIntro";
 import SocialShares from "./SocialShares";
+import TrendingPosts from "./TrendingPosts/TrendingPosts";
 
 const PostsAdditional = () => {
   return (
     <div className="posts-additional">
       <AvatarIntro />
       <SocialShares />
-      {/* <TrendingPosts />
-      <Writers /> */}
+      <TrendingPosts />
+      {/* <Writers /> */}
     </div>
   );
 };
---
+++ src/layout/Pages/Posts/PostsAdditional/TrendingPosts/TrendingPosts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageElementWrapper from "../../../PageElementWrapper";
+import Posts from "./Posts";
+
+const TrendingPosts = () => {
+  return (
+    <PageElementWrapper>
+      <Posts />
+      {/* <Tags /> */}
+    </PageElementWrapper>
+  );
+};
+
+export default TrendingPosts;

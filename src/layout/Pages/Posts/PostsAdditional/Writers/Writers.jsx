b946602152c5ff8f6669f826522f83099b1937b8
CJ Moro <scjmoro@gmail.com>
2018-12-08 18:23:18 -0800

    Add Writers component

---
+++ src/layout/Pages/Posts/PostsAdditional/Writers/Writers.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageElementWrapper from "../../../PageElementWrapper";
+import Writer from "./Writer";
+import Subscribe from "./Subscribe";
+
+const Writers = () => {
+  return (
+    <PageElementWrapper>
+      <Writer />
+      <Subscribe />
+    </PageElementWrapper>
+  );
+};
+
+export default Writers;

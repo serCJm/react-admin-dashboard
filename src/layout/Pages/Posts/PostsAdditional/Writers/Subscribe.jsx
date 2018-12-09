5a5b12e4fb4850a014832ddd86be6685eeec0dfc
CJ Moro <scjmoro@gmail.com>
2018-12-08 18:23:43 -0800

    Add Subscribe component

---
+++ src/layout/Pages/Posts/PostsAdditional/Writers/Subscribe.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <form action="#" className="fc subscribe">
+      <input
+        type="email"
+        name="subscribe"
+        id=""
+        placeholder="exmpl@exmpl.com"
+        className="subscribe-input"
+      />
+      <button className="subscribe-btn">Subscribe</button>
+    </form>
+  );
+};
+
+export default Subscribe;

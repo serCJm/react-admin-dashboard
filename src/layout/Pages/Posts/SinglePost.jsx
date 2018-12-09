f51c50fc086d378ee6c3bbf735d3b0876411e28e
CJ Moro <scjmoro@gmail.com>
2018-12-08 18:24:06 -0800

    Add SinglePost component

---
+++ src/layout/Pages/Posts/SinglePost.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageElementWrapper from "../PageElementWrapper";
+import SinglePostHeader from "./SinglePost/SinglePostHeader";
+
+const SinglePost = () => {
+  return (
+    <PageElementWrapper>
+      <SinglePostHeader />
+      {/* <SinglePostBody /> */}
+    </PageElementWrapper>
+  );
+};
+
+export default SinglePost;

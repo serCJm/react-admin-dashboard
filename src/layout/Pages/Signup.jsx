646e8108e45fd8ada5bab92ccf5b35a69e5e81d1
CJ Moro <scjmoro@gmail.com>
2018-12-11 05:50:01 -0800

    Add Signup component

--- src/layout/Pages/Signup.jsx
+++ src/layout/Pages/Signup.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
-import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faKey, faUser } from "@fortawesome/free-solid-svg-icons";
 import { AuthContext } from "../../components/Header/auth-context";
 
-class Login extends Component {
+class Signup extends Component {
   state = {};
 
   handleSubmit = (e, handleAuthentication) => {
@@ -18,31 +18,27 @@ class Login extends Component {
     return (
       <AuthContext.Consumer>
         {({ handleAuthentication }) => (
-          <div className="fc login-container">
+          <div className="fc signup-container">
             <div className="login-form">
-              <h1 className="fc m0 heading">Sign in to Dashboard</h1>
-              <div className="fcc social-login">
-                <p>You need to sign in to see the dashboard.</p>
-                <button className="p1 fsa btn--facebook pntr">
-                  <span className="mr1">
-                    <FontAwesomeIcon icon={faFacebookF} />
-                  </span>
-                  Sign in with Facebook
-                </button>
-                <button className="mtb1 p1 btn--twitter pntr">
-                  <span className="mr05">
-                    <FontAwesomeIcon icon={faTwitter} />
-                  </span>{" "}
-                  or with twitter
-                </button>
-              </div>
+              <h1 className="fc m0 heading">Sign Up</h1>
               <div className="p2 fcc form__account">
-                <p className="mt0">or use your account</p>
                 <form
                   className="fcc w100"
                   onSubmit={e => this.handleSubmit(e, handleAuthentication)}
                 >
-                  <div className="f mtb1 w100">
+                  <div className="f mtb05 w100">
+                    <span className="form__icon">
+                      <FontAwesomeIcon icon={faUser} />
+                    </span>
+                    <input
+                      type="text"
+                      name="user"
+                      placeholder="Username"
+                      className="p1 form__input"
+                      autoComplete="off"
+                    />
+                  </div>
+                  <div className="f mtb05 w100">
                     <span className="form__icon">
                       <FontAwesomeIcon icon={faEnvelope} />
                     </span>
@@ -54,7 +50,7 @@ class Login extends Component {
                       autoComplete="on"
                     />
                   </div>
-                  <div className="f mtb1 w100">
+                  <div className="f mtb05 w100">
                     <span className="form__icon">
                       <FontAwesomeIcon icon={faKey} />
                     </span>
@@ -67,12 +63,34 @@ class Login extends Component {
                     />
                   </div>
                   <div className="fsb form__actions">
-                    <Link to="/signup" className="link">
-                      Create a dashboard account
-                    </Link>
-                    <button className="btn btn--login pntr">Login</button>
+                    <button className="mtb1 btn btn--signup pntr">
+                      Create Account
+                    </button>
                   </div>
                 </form>
+              </div>
+              <div className="fcc social-login">
+                <p>SIGN UP WITH</p>
+                <div className="fsb w80">
+                  <button className="p1 btn__signup btn__signup--facebook pntr">
+                    <span className="mr05">
+                      <FontAwesomeIcon icon={faFacebookF} />
+                    </span>
+                    Facebook
+                  </button>
+                  <button className="p1 btn__signup btn__signup--twitter pntr">
+                    <span className="mr05">
+                      <FontAwesomeIcon icon={faTwitter} />
+                    </span>
+                    Twitter
+                  </button>
+                </div>
+                <p>
+                  Already have an account?{" "}
+                  <Link className="link" to="/login">
+                    Login
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
@@ -82,4 +100,4 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default Signup;

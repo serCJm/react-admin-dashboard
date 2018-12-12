aeed0470c722073c4b45d475748b77a6010516d7
CJ Moro <scjmoro@gmail.com>
2018-12-12 05:50:50 -0800

    Add InvoiceBody component

--- src/layout/Pages/Invoice/Invoice.jsx
+++ src/layout/Pages/Invoice/Invoice.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import PageElementWrapper from "../PageElementWrapper";
 import InvoiceHeader from "./InvoiceHeader/InvoiceHeader";
+import InvoiceBody from "./InvoiceBody/InvoiceBody";
 
 const Invoice = () => {
   return (
     <PageElementWrapper>
       <div className="invoice">
         <InvoiceHeader />
-        {/* <InvoiceBody />
-      <InvoiceFooter /> */}
+        <InvoiceBody />
+        {/* <InvoiceFooter /> */}
       </div>
     </PageElementWrapper>
   );
---
+++ src/layout/Pages/Invoice/InvoiceBody/InvoiceBody.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const InvoiceBody = () => {
+  return (
+    <div className="p2 table">
+      <p className="header cell">#</p>
+      <p className="header cell">Item</p>
+      <p className="header cell">Quantity</p>
+      <p className="header cell">Unit-Price</p>
+      <p className="header cell">Sub-total</p>
+      <p className="cell cell--odd">1</p>
+      <p className="cell cell--odd">Website wireframe</p>
+      <p className="cell cell--odd">5 hours</p>
+      <p className="cell cell--odd">$50</p>
+      <p className="cell cell--odd">$250</p>
+      <p className="cell">2</p>
+      <p className="cell">Graphic Design</p>
+      <p className="cell">15 hours</p>
+      <p className="cell">$50</p>
+      <p className="cell">$750</p>
+      <p className="cell cell--odd">3</p>
+      <p className="cell cell--odd">React Front-End Development</p>
+      <p className="cell cell--odd">20 hours</p>
+      <p className="cell cell--odd">$100</p>
+      <p className="cell cell--odd">$2,000</p>
+      <p className="cell">4</p>
+      <p className="cell">Node Back-End Development</p>
+      <p className="cell">25 hours</p>
+      <p className="cell">$100</p>
+      <p className="cell">$2,500</p>
+      <p className="cell cell--odd">5</p>
+      <p className="cell cell--odd">SEO and Keyword Research</p>
+      <p className="cell cell--odd">10 hours</p>
+      <p className="cell cell--odd">$50</p>
+      <p className="cell cell--odd">$500</p>
+      <div className="header cell" />
+      <div className="header cell" />
+      <div className="header cell" />
+      <p className="header cell">Total</p>
+      <p className="header cell">$6,000</p>
+    </div>
+  );
+};
+
+export default InvoiceBody;

62ce297aba37e0db4f95701420c5c5c629b853e8
CJ Moro <scjmoro@gmail.com>
2018-12-18 05:36:19 -0800

    Add conditional text

--- src/layout/Pages/Panels/components/BasicPanel/BasicPanel.jsx
+++ src/layout/Pages/Panels/components/BasicPanel/BasicPanel.jsx
@@ -3,16 +3,19 @@ import React from "react";
 const BasicPanel = props => {
   let title = null;
   if (props.title) title = <h1>{props.title}</h1>;
+  let text = (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus
+      tempor nulla, ut varius sem hendrerit vel. Cras sed interdum justo, sed
+      vestibulum risus. Aliquam erat volutpat. Proin hendrerit efficitur luctus.
+      In sit amet ex ut ligula vestibulum tristique in sit amet velit.
+    </p>
+  );
+  if (props.text) text = <p>{props.text}</p>;
   return (
     <div className="p2 panels__basic">
       {title}
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        dapibus tempor nulla, ut varius sem hendrerit vel. Cras sed interdum
-        justo, sed vestibulum risus. Aliquam erat volutpat. Proin hendrerit
-        efficitur luctus. In sit amet ex ut ligula vestibulum tristique in sit
-        amet velit.
-      </p>
+      {text}
     </div>
   );
 };

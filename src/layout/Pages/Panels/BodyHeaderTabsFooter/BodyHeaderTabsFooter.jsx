eac1da8c76d24147cde843e038b4aa99bc06a824
CJ Moro <scjmoro@gmail.com>
2018-12-18 05:36:50 -0800

    Add logic and PlainTabsHeader

--- src/layout/Pages/Panels/BodyHeaderTabsFooter/BodyHeaderTabsFooter.jsx
+++ src/layout/Pages/Panels/BodyHeaderTabsFooter/BodyHeaderTabsFooter.jsx
@@ -4,16 +4,53 @@ import HeaderFooter from "../components/HeaderFooter/HeaderFooter";
 import PlainTabsHeader from "../components/PlainTabsHeader/PlainTabsHeader";
 
 class BodyHeaderTabsFooter extends Component {
-  state = {};
+  state = {
+    active: "home",
+    tagTitle: null
+  };
+
+  handleSwitchActive = (id, e) => {
+    this.setState({ active: id, tagTitle: null });
+  };
+
+  handleSwitchTitle = (title, e) => {
+    this.setState({ tagTitle: title });
+  };
+
   render() {
+    let title;
+    let text;
+    if (this.state.active === "home") {
+      title = "Home";
+      text =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer vitae justo eget magna fermentum.";
+    } else if (this.state.active === "user") {
+      title = "User";
+      text =
+        "Nam at lectus urna duis. Maecenas sed enim ut sem viverra aliquet eget sit. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt.";
+    } else {
+      title = "Cog";
+      text =
+        "Arcu vitae elementum curabitur vitae nunc. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Mollis aliquam ut porttitor leo.";
+    }
+
+    if (this.state.tagTitle) {
+      title = this.state.tagTitle;
+    }
     return (
       <React.Fragment>
         <HeaderFooter
           heading="Panel Header + Plain Tabs"
           styleClass="c--purple"
         />
-        <PlainTabsHeader styleClass="c--purple" />
-        <BasicPanel />
+        <PlainTabsHeader
+          styleClass="c--purple"
+          dropdown
+          active={this.state.active}
+          handleSwitchActive={this.handleSwitchActive}
+          handleSwitchTitle={this.handleSwitchTitle}
+        />
+        <BasicPanel title={title} text={text} />
         <HeaderFooter
           heading="Panel Footer"
           miniHeading="Mini Heading"

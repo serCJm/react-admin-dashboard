114827848407d3f97bfb225f63703b8163eb76b1
CJ Moro <scjmoro@gmail.com>
2018-12-20 06:14:24 -0800

    Add new panels

--- src/layout/Pages/Panels/Panels.jsx
+++ src/layout/Pages/Panels/Panels.jsx
@@ -8,34 +8,62 @@ import BodyHeaderTabsFooter from "./BodyHeaderTabsFooter/BodyHeaderTabsFooter";
 import HeaderBodyTabsFooter from "./HeaderBodyTabsFooter/HeaderBodyTabsFooter";
 import HeaderTabsBodyTabsFooter from "./HeaderTabsBodyTabsFooter/HeaderTabsBodyTabsFooter";
 import Tabs2Body from "./Tabs2Body/Tabs2Body";
+import BodyTabs2 from "./BodyTabs2/BodyTabs2";
+import Tabs2BodyTabs2 from "./Tabs2BodyTabs2/Tabs2BodyTabs2";
+import Tabs2SideBody from "./Tabs2SideBody/Tabs2SideBody";
+import BodyTabs2Side from "./BodyTabs2Side/BodyTabs2Side";
+import Tabs2SideBodyTabs2Side from "./Tabs2SideBodyTabs2Side/Tabs2SideBodyTabs2Side";
 
 const Panels = () => {
   return (
     <div className="panels">
-      <PageElementWrapper>
-        <BasicPanel title="Basic Panel" />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <BodyWithFooter />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <BodyWithHeader />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <BodyHeaderFooter />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <BodyHeaderTabsFooter />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <HeaderBodyTabsFooter />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <HeaderTabsBodyTabsFooter />
-      </PageElementWrapper>
-      <PageElementWrapper>
-        <Tabs2Body />
-      </PageElementWrapper>
+      <div className="three-column-grid">
+        <PageElementWrapper>
+          <BasicPanel title="Basic Panel" />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <BodyWithFooter />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <BodyWithHeader />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <BodyHeaderFooter />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <BodyHeaderTabsFooter />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <HeaderBodyTabsFooter />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <HeaderTabsBodyTabsFooter />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <Tabs2Body />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <BodyTabs2 />
+        </PageElementWrapper>
+      </div>
+      <div className="mtb2 two-column-grid">
+        <div className="panel--long">
+          <PageElementWrapper>
+            <Tabs2BodyTabs2 />
+          </PageElementWrapper>
+        </div>
+        <PageElementWrapper>
+          <Tabs2SideBody />
+        </PageElementWrapper>
+        <PageElementWrapper>
+          <BodyTabs2Side />
+        </PageElementWrapper>
+        <div className="panel--long">
+          <PageElementWrapper>
+            <Tabs2SideBodyTabs2Side />
+          </PageElementWrapper>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/layout/Pages/Panels/Tabs2BodyTabs2/Tabs2BodyTabs2.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import BasicPanel from "../components/BasicPanel/BasicPanel";
+import PlainTabsStyle2 from "../components/PlainTabsStyle2/PlainTabsStyle2";
+
+class Tabs2BodyTabs2 extends Component {
+  state = {
+    active: "home",
+    tagTitle: null
+  };
+
+  handleSwitchActive = (id, e) => {
+    this.setState({ active: id, tagTitle: null });
+  };
+
+  handleSwitchTitle = (title, e) => {
+    this.setState({ tagTitle: title });
+  };
+
+  render() {
+    let title;
+    let text;
+    if (this.state.active === "home") {
+      title = "Home";
+      text =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer vitae justo eget magna fermentum.";
+    } else if (this.state.active === "user") {
+      title = "User";
+      text =
+        "Nam at lectus urna duis. Maecenas sed enim ut sem viverra aliquet eget sit. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt.";
+    } else if (this.state.active === "cog") {
+      title = "Cog";
+      text =
+        "Arcu vitae elementum curabitur vitae nunc. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Mollis aliquam ut porttitor leo.";
+    } else if (this.state.active === "home (footer)") {
+      title = "Home (Footer)";
+      text =
+        "Arcu vitae elementum curabitur vitae nunc. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Mollis aliquam ut porttitor leo.";
+    } else if (this.state.active === "user (footer)") {
+      title = "User (Footer)";
+      text =
+        "Arcu vitae elementum curabitur vitae nunc. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Mollis aliquam ut porttitor leo.";
+    } else if (this.state.active === "cog (footer)") {
+      title = "Cog (Footer)";
+      text =
+        "Arcu vitae elementum curabitur vitae nunc. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Mollis aliquam ut porttitor leo.";
+    }
+
+    if (this.state.tagTitle) {
+      title = this.state.tagTitle;
+    }
+    return (
+      <React.Fragment>
+        <PlainTabsStyle2
+          styleClass="c--lightBlue"
+          active={this.state.active}
+          handleSwitchActive={this.handleSwitchActive}
+        />
+        <BasicPanel title={title} text={text} />
+        <PlainTabsStyle2
+          styleClass="c--Blue"
+          active={this.state.active}
+          handleSwitchActive={this.handleSwitchActive}
+          iconSet2
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Tabs2BodyTabs2;

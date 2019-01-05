6ce3cd4bb6f0a48dfdd8245fad4f2d7d0bfdabe9
CJ Moro <scjmoro@gmail.com>
2019-01-05 06:41:33 -0800

    Comment out unused menus

--- src/components/SideMenu/SideMenuHomeComponents.jsx
+++ src/components/SideMenu/SideMenuHomeComponents.jsx
@@ -60,201 +60,201 @@ class SideMenuHomeComponents extends Component {
                   id: "line-series",
                   icon: null,
                   text: "Line Series"
-                },
-                {
-                  id: "area-series",
-                  icon: null,
-                  text: "Area Series"
-                },
-                {
-                  id: "bar-column-series",
-                  icon: null,
-                  text: "Bar+Column Series"
-                },
-                {
-                  id: "mixed",
-                  icon: null,
-                  text: "Mixed Series"
-                },
-                {
-                  id: "pie-donut-series",
-                  icon: null,
-                  text: "Pie+Donut Series"
                 }
+                // {
+                //   id: "area-series",
+                //   icon: null,
+                //   text: "Area Series"
+                // },
+                // {
+                //   id: "bar-column-series",
+                //   icon: null,
+                //   text: "Bar+Column Series"
+                // },
+                // {
+                //   id: "mixed",
+                //   icon: null,
+                //   text: "Mixed Series"
+                // },
+                // {
+                //   id: "pie-donut-series",
+                //   icon: null,
+                //   text: "Pie+Donut Series"
+                // }
               ]
-            },
-            {
-              id: "chart-js",
-              icon: <FontAwesomeIcon icon={faChartBar} />,
-              text: "Chart.JS"
-            },
-            {
-              id: "c3-js",
-              icon: <FontAwesomeIcon icon={faChartArea} />,
-              text: "C3.JS"
-            },
-            {
-              id: "morris-js",
-              icon: <FontAwesomeIcon icon={faSignal} />,
-              text: "Morris.JS"
             }
+            // {
+            //   id: "chart-js",
+            //   icon: <FontAwesomeIcon icon={faChartBar} />,
+            //   text: "Chart.JS"
+            // },
+            // {
+            //   id: "c3-js",
+            //   icon: <FontAwesomeIcon icon={faChartArea} />,
+            //   text: "C3.JS"
+            // },
+            // {
+            //   id: "morris-js",
+            //   icon: <FontAwesomeIcon icon={faSignal} />,
+            //   text: "Morris.JS"
+            // }
           ]
-        },
-        {
-          id: "static-timeline",
-          icon: <FontAwesomeIcon icon={faClock} />,
-          text: "Static Timeline",
-          submenu: null
-        },
-        {
-          id: "interactive-timeline",
-          icon: <FontAwesomeIcon icon={faStopwatch} />,
-          text: "Interactive Timeline",
-          submenu: null
-        },
-        {
-          id: "codemirror",
-          icon: <FontAwesomeIcon icon={faCode} />,
-          text: "Codemirror",
-          submenu: null
-        },
-        {
-          id: "maps",
-          icon: <FontAwesomeIcon icon={faMapMarkerAlt} />,
-          text: "Maps",
-          submenu: null
-        },
-        {
-          id: "editor",
-          icon: <FontAwesomeIcon icon={faPenSquare} />,
-          text: "Editor",
-          submenu: null
-        },
-        {
-          id: "ui-elements",
-          icon: <FontAwesomeIcon icon={faCompass} />,
-          text: "UI Elements",
-          countstyle: { color: "#fff", background: "rgb(215, 31, 75)" },
-          submenu: [
-            {
-              id: "buttons",
-              icon: <FontAwesomeIcon icon={faPlayCircle} />,
-              text: "Buttons"
-            },
-            {
-              id: "dropdowns",
-              icon: <FontAwesomeIcon icon={faChevronCircleDown} />,
-              text: "Dropdowns"
-            },
-            {
-              id: "tabs-navs",
-              icon: <FontAwesomeIcon icon={faTable} />,
-              text: "Tabs & Navs"
-            },
-            {
-              id: "sliders",
-              icon: <FontAwesomeIcon icon={faSlidersH} />,
-              text: "Sliders"
-            },
-            {
-              id: "knobs",
-              icon: <FontAwesomeIcon icon={faAdjust} />,
-              text: "Knobs"
-            },
-            {
-              id: "modals",
-              icon: <FontAwesomeIcon icon={faBars} />,
-              text: "Modals"
-            },
-            {
-              id: "messanger",
-              icon: <FontAwesomeIcon icon={faComment} />,
-              text: "Messanger"
-            }
-          ]
-        },
-        {
-          id: "forms",
-          icon: <FontAwesomeIcon icon={faHandPointRight} />,
-          text: "Forms",
-          countstyle: { color: "#fff", background: "rgb(217, 83, 79)" },
-          submenu: [
-            {
-              id: "controls",
-              icon: <FontAwesomeIcon icon={faGamepad} />,
-              text: "Controls"
-            },
-            {
-              id: "x-editable",
-              icon: <FontAwesomeIcon icon={faEdit} />,
-              text: "X-Editable"
-            },
-            {
-              id: "wizard",
-              icon: <FontAwesomeIcon icon={faMagic} />,
-              text: "Wizard"
-            }
-          ]
-        },
-        {
-          id: "tables",
-          icon: <FontAwesomeIcon icon={faTable} />,
-          text: "Tables",
-          countstyle: { color: "#fff", background: "rgb(121, 176, 236)" },
-          submenu: [
-            {
-              id: "bootstrap",
-              icon: <FontAwesomeIcon icon={faTv} />,
-              text: "Bootstrap Tables"
-            },
-            {
-              id: "datatables",
-              icon: <FontAwesomeIcon icon={faCompactDisc} />,
-              text: "Datatables"
-            },
-            {
-              id: "tablesaw",
-              icon: <FontAwesomeIcon icon={faSun} />,
-              text: "Tablesaw"
-            }
-          ]
-        },
-        {
-          id: "grid",
-          icon: <FontAwesomeIcon icon={faThLarge} />,
-          text: "Grid",
-          submenu: null
-        },
-        {
-          id: "calendar",
-          icon: <FontAwesomeIcon icon={faCalendarAlt} />,
-          text: "Calendar",
-          submenu: null
-        },
-        {
-          id: "file-utilities",
-          icon: <FontAwesomeIcon icon={faFile} />,
-          text: "File Utilities",
-          countstyle: { color: "#fff", background: "rgb(255, 196, 151)" },
-          submenu: [
-            {
-              id: "dropzone",
-              icon: <FontAwesomeIcon icon={faDownload} />,
-              text: "Dropzone"
-            },
-            {
-              id: "image-cropping",
-              icon: <FontAwesomeIcon icon={faImages} />,
-              text: "Image Cropping"
-            }
-          ]
-        },
-        {
-          id: "fonts",
-          icon: <FontAwesomeIcon icon={faFont} />,
-          text: "Fonts",
-          submenu: null
         }
+        // {
+        //   id: "static-timeline",
+        //   icon: <FontAwesomeIcon icon={faClock} />,
+        //   text: "Static Timeline",
+        //   submenu: null
+        // },
+        // {
+        //   id: "interactive-timeline",
+        //   icon: <FontAwesomeIcon icon={faStopwatch} />,
+        //   text: "Interactive Timeline",
+        //   submenu: null
+        // },
+        // {
+        //   id: "codemirror",
+        //   icon: <FontAwesomeIcon icon={faCode} />,
+        //   text: "Codemirror",
+        //   submenu: null
+        // },
+        // {
+        //   id: "maps",
+        //   icon: <FontAwesomeIcon icon={faMapMarkerAlt} />,
+        //   text: "Maps",
+        //   submenu: null
+        // },
+        // {
+        //   id: "editor",
+        //   icon: <FontAwesomeIcon icon={faPenSquare} />,
+        //   text: "Editor",
+        //   submenu: null
+        // },
+        // {
+        //   id: "ui-elements",
+        //   icon: <FontAwesomeIcon icon={faCompass} />,
+        //   text: "UI Elements",
+        //   countstyle: { color: "#fff", background: "rgb(215, 31, 75)" },
+        //   submenu: [
+        //     {
+        //       id: "buttons",
+        //       icon: <FontAwesomeIcon icon={faPlayCircle} />,
+        //       text: "Buttons"
+        //     },
+        //     {
+        //       id: "dropdowns",
+        //       icon: <FontAwesomeIcon icon={faChevronCircleDown} />,
+        //       text: "Dropdowns"
+        //     },
+        //     {
+        //       id: "tabs-navs",
+        //       icon: <FontAwesomeIcon icon={faTable} />,
+        //       text: "Tabs & Navs"
+        //     },
+        //     {
+        //       id: "sliders",
+        //       icon: <FontAwesomeIcon icon={faSlidersH} />,
+        //       text: "Sliders"
+        //     },
+        //     {
+        //       id: "knobs",
+        //       icon: <FontAwesomeIcon icon={faAdjust} />,
+        //       text: "Knobs"
+        //     },
+        //     {
+        //       id: "modals",
+        //       icon: <FontAwesomeIcon icon={faBars} />,
+        //       text: "Modals"
+        //     },
+        //     {
+        //       id: "messanger",
+        //       icon: <FontAwesomeIcon icon={faComment} />,
+        //       text: "Messanger"
+        //     }
+        //   ]
+        // },
+        // {
+        //   id: "forms",
+        //   icon: <FontAwesomeIcon icon={faHandPointRight} />,
+        //   text: "Forms",
+        //   countstyle: { color: "#fff", background: "rgb(217, 83, 79)" },
+        //   submenu: [
+        //     {
+        //       id: "controls",
+        //       icon: <FontAwesomeIcon icon={faGamepad} />,
+        //       text: "Controls"
+        //     },
+        //     {
+        //       id: "x-editable",
+        //       icon: <FontAwesomeIcon icon={faEdit} />,
+        //       text: "X-Editable"
+        //     },
+        //     {
+        //       id: "wizard",
+        //       icon: <FontAwesomeIcon icon={faMagic} />,
+        //       text: "Wizard"
+        //     }
+        //   ]
+        // },
+        // {
+        //   id: "tables",
+        //   icon: <FontAwesomeIcon icon={faTable} />,
+        //   text: "Tables",
+        //   countstyle: { color: "#fff", background: "rgb(121, 176, 236)" },
+        //   submenu: [
+        //     {
+        //       id: "bootstrap",
+        //       icon: <FontAwesomeIcon icon={faTv} />,
+        //       text: "Bootstrap Tables"
+        //     },
+        //     {
+        //       id: "datatables",
+        //       icon: <FontAwesomeIcon icon={faCompactDisc} />,
+        //       text: "Datatables"
+        //     },
+        //     {
+        //       id: "tablesaw",
+        //       icon: <FontAwesomeIcon icon={faSun} />,
+        //       text: "Tablesaw"
+        //     }
+        //   ]
+        // },
+        // {
+        //   id: "grid",
+        //   icon: <FontAwesomeIcon icon={faThLarge} />,
+        //   text: "Grid",
+        //   submenu: null
+        // },
+        // {
+        //   id: "calendar",
+        //   icon: <FontAwesomeIcon icon={faCalendarAlt} />,
+        //   text: "Calendar",
+        //   submenu: null
+        // },
+        // {
+        //   id: "file-utilities",
+        //   icon: <FontAwesomeIcon icon={faFile} />,
+        //   text: "File Utilities",
+        //   countstyle: { color: "#fff", background: "rgb(255, 196, 151)" },
+        //   submenu: [
+        //     {
+        //       id: "dropzone",
+        //       icon: <FontAwesomeIcon icon={faDownload} />,
+        //       text: "Dropzone"
+        //     },
+        //     {
+        //       id: "image-cropping",
+        //       icon: <FontAwesomeIcon icon={faImages} />,
+        //       text: "Image Cropping"
+        //     }
+        //   ]
+        // },
+        // {
+        //   id: "fonts",
+        //   icon: <FontAwesomeIcon icon={faFont} />,
+        //   text: "Fonts",
+        //   submenu: null
+        // }
       ],
       classNames: {
         title: "title",

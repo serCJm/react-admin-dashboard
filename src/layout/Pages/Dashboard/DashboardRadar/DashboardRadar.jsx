4b49cbf0a080164b07183c88d940ca4f72872e04
CJ Moro <scjmoro@gmail.com>
2019-01-05 06:30:10 -0800

    Add Container Dimensions

--- src/layout/Pages/Dashboard/DashboardRadar/DashboardRadar.jsx
+++ src/layout/Pages/Dashboard/DashboardRadar/DashboardRadar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import DashboardRadarGraph from "./DashboardRadarGraph";
 import DashboardRadarTable from "./DashboardRadarTable";
+import ContainerDimensions from "react-container-dimensions";
 const DashboardRadar = () => {
   return (
     <div className="dashboard-radar">
-      <DashboardRadarGraph />
+      <ContainerDimensions>
+        {({ width }) => <DashboardRadarGraph width={width - 150} />}
+      </ContainerDimensions>
+
       <DashboardRadarTable />
     </div>
   );

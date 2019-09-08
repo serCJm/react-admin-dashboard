3f5c7a02392b22a1b23fcd025cab479ae21029f8
cjmoro <scjmoro@gmail.com>
2019-09-08 00:27:03 -0700

    Refactor into functional component

--- src/layout/Pages/Pages.jsx
+++ src/layout/Pages/Pages.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Suspense } from "react";
 import { Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 const Posts = React.lazy(() => import("./Posts/Posts"));
 const Inbox = React.lazy(() => import("./Mailbox/Inbox/Inbox"));
@@ -16,49 +17,26 @@ const LineSeries = React.lazy(() =>
   import("./Charts/D3/LineSeries/LineSeries")
 );
 
-class Pages extends Component {
-  state = {
-    mobile: window.matchMedia("(max-width: 800px)").matches
-  };
-
-  hangleScreenResize = () => {
-    const mq = window.matchMedia("(max-width: 800px)").matches;
-    this.setState({ mobile: mq });
-  };
-
-  componentDidMount() {
-    window.addEventListener("resize", this.hangleScreenResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.hangleScreenResize);
-  }
-
-  render() {
-    const { sideMenu } = this.props;
-    let mainClass = "pages sidemenu-open";
-    if (!sideMenu && window.matchMedia("(max-width: 800px)").matches) {
-      mainClass = "pages";
-    }
-    return (
-      <main className={mainClass}>
-        <Route exact path="/" component={Dashboard} />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Route exact path="/inbox" render={() => <Inbox />} />
-          <Route exact path="/mail" render={() => <Mail />} />
-          <Route exact path="/compose" render={() => <Compose />} />
-          <Route exact path="/gallery" render={() => <Gallery />} />
-          <Route exact path="/social" render={() => <Social />} />
-          <Route exact path="/posts" render={() => <Posts />} />
-          <Route exact path="/singlePost" render={() => <SinglePost />} />
-          <Route exact path="/invoice" render={() => <Invoice />} />
-          <Route exact path="/pricing-tables" render={() => <Pricing />} />
-          <Route exact path="/panels" render={() => <Panels />} />
-          <Route exact path="/line-series" render={() => <LineSeries />} />
-        </Suspense>
-      </main>
-    );
-  }
-}
+const Pages = ({ sideMenu }) => {
+  let mainClass = sideMenu ? "pages sidemenu-open" : "pages";
+  return (
+    <main className={mainClass}>
+      <Route exact path="/" component={Dashboard} />
+      <Suspense fallback={<Spinner></Spinner>}>
+        <Route exact path="/inbox" render={() => <Inbox />} />
+        <Route exact path="/mail" render={() => <Mail />} />
+        <Route exact path="/compose" render={() => <Compose />} />
+        <Route exact path="/gallery" render={() => <Gallery />} />
+        <Route exact path="/social" render={() => <Social />} />
+        <Route exact path="/posts" render={() => <Posts />} />
+        <Route exact path="/singlePost" render={() => <SinglePost />} />
+        <Route exact path="/invoice" render={() => <Invoice />} />
+        <Route exact path="/pricing-tables" render={() => <Pricing />} />
+        <Route exact path="/panels" render={() => <Panels />} />
+        <Route exact path="/line-series" render={() => <LineSeries />} />
+      </Suspense>
+    </main>
+  );
+};
 
 export default Pages;

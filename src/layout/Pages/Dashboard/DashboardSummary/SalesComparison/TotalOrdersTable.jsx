41c87f8008fcf134fb735c0161a3d4678c2c96ce
cjmoro <scjmoro@gmail.com>
2019-09-11 23:46:00 -0700

    Hard code data

--- src/layout/Pages/Dashboard/DashboardSummary/SalesComparison/TotalOrdersTable.jsx
+++ src/layout/Pages/Dashboard/DashboardSummary/SalesComparison/TotalOrdersTable.jsx
@@ -4,17 +4,17 @@ import TotalOrdersTableRow from "./TotalOrdersTableRow";
 const content = [
   {
     total: "9,999",
-    data: Array.from({ length: 15 }, () => Math.floor(Math.random() * 20)),
+    data: [12, 9, 10, 15, 7, 19, 6, 16, 5, 14, 15, 10, 19, 5, 16],
     percent: "+10%"
   },
   {
     total: "1,500",
-    data: Array.from({ length: 15 }, () => Math.floor(Math.random() * 20)),
+    data: [9, 9, 18, 16, 1, 1, 6, 19, 8, 9, 4, 1, 6, 6, 9],
     percent: "0%"
   },
   {
     total: "3,000",
-    data: Array.from({ length: 15 }, () => Math.floor(Math.random() * 20)),
+    data: [18, 16, 14, 9, 15, 5, 9, 12, 15, 7, 6, 3, 6, 19, 19],
     percent: "-70%"
   }
 ];

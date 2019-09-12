2ebdfa86a392c84aa17b392c7aad2c4b46819fb8
cjmoro <scjmoro@gmail.com>
2019-09-11 23:12:50 -0700

    Change Main component import

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContext } from "./components/Header/auth-context";
 import Spinner from "./components/UI/Spinner/Spinner";
+import Main from "./Main";
 
-
-const Lock = React.lazy(() => import("./layout/Pages/Lock"))
-const Login = React.lazy(() => import("./layout/Pages/Login"))
-const Signup = React.lazy(() => import("./layout/Pages/Signup"))
-const Main = React.lazy(() => import("./Main"))
+const Lock = React.lazy(() => import("./layout/Pages/Lock"));
+const Login = React.lazy(() => import("./layout/Pages/Login"));
+const Signup = React.lazy(() => import("./layout/Pages/Signup"));
+//const Main = React.lazy(() => import("./Main"))
 
 class App extends Component {
   handleAuthentication = () => {
@@ -49,12 +49,12 @@ class App extends Component {
       );
     }
     return (
-      <Suspense fallback={<Spinner/>}>
-      <BrowserRouter>
-        <AuthContext.Provider value={this.state}>
-          {content}
-        </AuthContext.Provider>
-      </BrowserRouter>
+      <Suspense fallback={<Spinner />}>
+        <BrowserRouter>
+          <AuthContext.Provider value={this.state}>
+            {content}
+          </AuthContext.Provider>
+        </BrowserRouter>
       </Suspense>
     );
   }

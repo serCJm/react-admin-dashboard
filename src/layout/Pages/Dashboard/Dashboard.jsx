f4497434cc094de79e385f199ddcbaac1fdcbe16
cjmoro <scjmoro@gmail.com>
2019-09-11 22:53:13 -0700

    Comment out Navigation

--- src/layout/Pages/Dashboard/Dashboard.jsx
+++ src/layout/Pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import DashboardOverview from "./DashboardOverview/DashboardOverview";
 import DashboardRadar from "./DashboardRadar/DashboardRadar";
 import DashboardSummary from "./DashboardSummary/DashboardSummary";
 import DashboardCalendarTime from "./DashboardCalendarTime/DashboardCalendarTime";
-import Navigation from "./Navigation/Navigation";
+//import Navigation from "./Navigation/Navigation";
 
 const Dashboard = () => {
   return (

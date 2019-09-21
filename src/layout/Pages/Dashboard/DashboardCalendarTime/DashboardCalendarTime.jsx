813c8d3677c5b165a4fc8ed662102ed3b8e02bde
cjmoro <scjmoro@gmail.com>
2019-09-21 15:25:32 -0700

    Re-write to use Hooks

--- src/layout/Pages/Dashboard/DashboardCalendarTime/DashboardCalendarTime.jsx
+++ src/layout/Pages/Dashboard/DashboardCalendarTime/DashboardCalendarTime.jsx
@@ -1,57 +1,100 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import Calendar from "react-calendar";
 import Time from "./Time";
 import TimeToggle from "./TimeToggle";
 import WeatherInfo from "./WeatherInfo";
 
-class DashboardCalendarTime extends Component {
-  state = {
-    date: new Date(),
-    displayTimePicker: false
+const DashboardCalendarTime = () => {
+  const [date, setDate] = useState(new Date());
+  const [displayTimePicker, setDisplayTimePicker] = useState(false);
+
+  const handleTimePicker = () => {
+    setDisplayTimePicker(prevState => !prevState);
   };
 
-  handleTimePicker = () => {
-    this.setState(prevState => ({
-      displayTimePicker: !prevState.displayTimePicker
-    }));
-  };
+  const onChange = date => setDate(date);
 
-  onChange = date => this.setState({ date });
-  render() {
-    return (
-      <div className="calendar-time">
-        <div
-          style={
-            this.state.displayTimePicker
-              ? {
-                  maxHeight: "0",
-                  overflowY: "hidden",
-                  transition: "max-height 0.3s linear"
-                }
-              : {
-                  maxHeight: "30rem",
-                  transition: "max-height 0.3s linear"
-                }
-          }
-        >
-          <Calendar
-            className="calendar"
-            onChange={this.onChange}
-            value={this.state.date}
-          />
-        </div>
-        <TimeToggle
-          handleTimePicker={this.handleTimePicker}
-          displayTimePicker={this.state.displayTimePicker}
-        />
-        <Time
-          handleTimePicker={this.handleTimePicker}
-          displayTimePicker={this.state.displayTimePicker}
-        />
-        <WeatherInfo />
+  return (
+    <div className="calendar-time">
+      <div
+        style={
+          displayTimePicker
+            ? {
+                maxHeight: "0",
+                overflowY: "hidden",
+                transition: "max-height 0.3s linear"
+              }
+            : {
+                maxHeight: "30rem",
+                transition: "max-height 0.3s linear"
+              }
+        }
+      >
+        <Calendar className="calendar" onChange={onChange} value={date} />
       </div>
-    );
-  }
-}
+      <TimeToggle
+        handleTimePicker={handleTimePicker}
+        displayTimePicker={displayTimePicker}
+      />
+      <Time
+        handleTimePicker={handleTimePicker}
+        displayTimePicker={displayTimePicker}
+      />
+      <WeatherInfo />
+    </div>
+  );
+};
 
 export default DashboardCalendarTime;
+
+// class DashboardCalendarTime extends Component {
+//   state = {
+//     date: new Date(),
+//     displayTimePicker: false
+//   };
+
+//   handleTimePicker = () => {
+//     this.setState(prevState => ({
+//       displayTimePicker: !prevState.displayTimePicker
+//     }));
+//   };
+
+//   onChange = date => this.setState({ date });
+//   render() {
+//     return (
+//       <div className="calendar-time">
+//         <div
+//           style={
+//             this.state.displayTimePicker
+//               ? {
+//                   maxHeight: "0",
+//                   overflowY: "hidden",
+//                   transition: "max-height 0.3s linear"
+//                 }
+//               : {
+//                   maxHeight: "30rem",
+//                   transition: "max-height 0.3s linear"
+//                 }
+//           }
+//         >
+//           <Calendar
+//             className="calendar"
+//             onChange={this.onChange}
+//             value={this.state.date}
+//           />
+//         </div>
+//         <TimeToggle
+//           handleTimePicker={this.handleTimePicker}
+//           displayTimePicker={this.state.displayTimePicker}
+//         />
+//         <Time
+//           handleTimePicker={this.handleTimePicker}
+//           displayTimePicker={this.state.displayTimePicker}
+//         />
+//         <WeatherInfo />
+//       </div>
+//     );
+//   }
+// }
+
+// export default DashboardCalendarTime;

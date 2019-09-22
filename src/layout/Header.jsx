c08561ca7064c4ef716d7d9796b20951ab89ba5e
cjmoro <scjmoro@gmail.com>
2019-09-21 20:46:32 -0700

    Remove screen size event listener

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,43 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import HeaderTopMenu from "../components/Header/HeaderTopMenu";
 import SideMenu from "../components/SideMenu/SideMenu";
 import HeaderMenuBtn from "../components/Header/HeaderMenuBtn";
 
-class Header extends Component {
-  state = {
-    mobile: window.matchMedia("(max-width: 800px)").matches
-  };
-
-  hangleScreenResize = () => {
-    const mq = window.matchMedia("(max-width: 800px)").matches;
-    this.setState({ mobile: mq });
-  };
-
-  componentDidMount() {
-    window.addEventListener("resize", this.hangleScreenResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.hangleScreenResize);
-  }
-
-  render() {
-    let headerMenuBtn = null;
-    if (this.state.mobile) {
-      headerMenuBtn = <HeaderMenuBtn onClick={this.props.toggleSideMenu} />;
-    }
-    return (
-      <header>
-        <HeaderTopMenu
-          logo="Logo"
-          headerMenuBtn={headerMenuBtn}
-          sideMenu={this.props.sideMenu}
-          mobile={this.state.mobile}
-        />
-        <SideMenu sideMenu={this.props.sideMenu} mobile={this.state.mobile} />
-      </header>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <header>
+      <HeaderTopMenu
+        logo="Logo"
+        headerMenuBtn={<HeaderMenuBtn onClick={props.toggleSideMenu} />}
+        sideMenu={props.sideMenu}
+      />
+      <SideMenu sideMenu={props.sideMenu} />
+    </header>
+  );
+};
 
 export default Header;
